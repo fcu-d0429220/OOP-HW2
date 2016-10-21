09cc530908893949ae26331cd7fc90f7b5a8f390
user <user@FF05>
2016-10-21 10:00:50 +0800

    :D

---
+++ src/Continue.java
@@ -0,0 +1,12 @@
+
+public class Continue {
+	public static void main(String[] args){
+		
+		for (int i = 0; i<5; i++){
+			if(i % 2 != 0){
+				System.out.println(i); 
+				continue;
+			}
+		}
+	}
+}
--- src/StringComparator.java
+++ src/StringComparator.java
@@ -4,19 +4,19 @@ public class StringComparator {
 	public static void main(String[] args) {
 		String str1, str2;
 		Scanner sc = new Scanner (System.in);
-		
-		System.out.println("Enter string 1: ");
-		str1 = sc.nextLine();
-		System.out.println("Enter string 2: ");
-		str2 = sc.nextLine();
-		
-		if (str1.toUpperCase().equals(str2.toUpperCase())) {
-			System.out.printf("The two strings are the same.");
-		}
-		
-		else {
-			System.out.printf("The two strings are not the same.");
-		}
+		do{
+			System.out.println("Enter string 1: ");
+			str1 = sc.nextLine();
+			System.out.println("Enter string 2: ");
+			str2 = sc.nextLine();
+			
+			if (str1.toUpperCase().equals(str2.toUpperCase())) {
+				System.out.printf("The two strings are the same.");
+			}
+			
+			else {
+				System.out.printf("The two strings are not the same.\n\n");
+			}
+		}while (!str1.equals(str2));
 	}
-
 }
